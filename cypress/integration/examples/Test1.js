1f8bdb22ff1ef18393396981a4ed60def25fac68
Juan Nicolas <jnicoosses@gmail.com>
2022-07-12 10:22:19 -0300

    First suite example
    Cy.visit example

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,15 @@
+describe('My first test suite', function()
+{
+   it('My first test case',function() {
+
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+
+   })
+
+//    it('My second test case',function() {
+
+//     //test step
+
+//    })
+
+} )
