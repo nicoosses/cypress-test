b7345f15684194c94fd4f32f7d313a7d74f36b3f
Juan Nicolas <jnicoosses@gmail.com>
2022-07-12 11:29:28 -0300

    Filtering by visible product and selecting one
    When an element its invisible, we can filter telling cypress that get a '.product' with property :visible like line 14.

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -2,22 +2,23 @@
 
 describe('My first test suite', function()
 {
-    it('My first test case',function() {
+    it('1- My first test case',function() {
 
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 
    })
 
-   it('Type something to search in search bar', function() {
+   it('2- Type something to search in search bar', function() {
         cy.get('.search-keyword').type('ca');
         cy.wait(2000)
-        cy.get('.product').should('have.length',4)
+        cy.get('.product:visible').should('have.length',4)
    })
 
-//    it('My second test case',function() {
+   it('3- Select one product', function(){
 
-//     //test step
+    cy.get(':nth-child(2) > .product-action > button').click();
 
-//    })
+   })
+
 
 } )
