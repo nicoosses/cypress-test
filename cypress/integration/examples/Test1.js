75cce3eb762ec0a3da188769498a5ebf4eb0520e
Juan Nicolas <jnicoosses@gmail.com>
2022-07-12 11:21:58 -0300

    First Fail path
    get and type (type in search bar)
    wait (wait for results)
    get and assertion (validate)

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,9 +1,17 @@
+
+
 describe('My first test suite', function()
 {
-   it('My first test case',function() {
+    it('My first test case',function() {
 
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 
+   })
+
+   it('Type something to search in search bar', function() {
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000)
+        cy.get('.product').should('have.length',4)
    })
 
 //    it('My second test case',function() {
