bf0b559a4639456c9cce1e95382539961c2f48b4
Juan Nicolas <jnicoosses@gmail.com>
2022-07-13 07:21:39 -0300

    Three ways to select one product in table
    Theres many ways to select a product in a table.
    Here are three ways to do it.
    .
    First: Using unique css selectors.
    .
    Second: Filtering elements between father and child classes using Find, equal, and contains to determine conditions.
    .
    Third: Iterating the father component using it as array structure and finding elements by determinated conditions.
    Using each and conditions inside of it.
    .
    Anotation: $el its asynch. So we need to use Wrap to work with some methods like Click.

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -11,14 +11,33 @@ describe('My first test suite', function()
    it('2- Type something to search in search bar', function() {
         cy.get('.search-keyword').type('ca');
         cy.wait(2000)
+        /*two ways to select products of a list (like ecommerce): first of all is filtering all the products and select only products with attribute visible*/
         cy.get('.product:visible').should('have.length',4)
+        //the second way is using find() inside the list elements who contains .product class, entering through the class name to their child elements
+        cy.get('.products').find('.product').should('have.length',4)
+
+        
    })
+
 
    it('3- Select one product', function(){
+          // //three ways: the first is selecting the eleemnt by it unique css selector 
+          cy.get(':nth-child(1) > .product-action > button').click();
+          // //the second is filtering elements using find, equal and contains 
+          cy.get('.products').find('.product').eq(0).contains('ADD TO CART').click();
 
-    cy.get(':nth-child(2) > .product-action > button').click();
+          //the third way is the optimous using each to iterate in a "table" selecting 1 element and working uniquely on it.
+          
+          cy.get('.products').as('productLocator')
+          cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+          //we iterate elements .product inside the father element with class .products
+               const textCard=$el.find('h4.product-name').text()  
+               const productPrice=$el.find('p.product-price').text()   
+                    if(textCard.startsWith('C') & productPrice > 56 & productPrice < 600)
+                         {
+                              cy.wrap($el).find('button').click();
+                         }
+          })
 
-   })
-
-   
-} )
+     })
+})
